7a8152de03b32c186625ce9129bf746aecea75fb
caleblareau <caleblareau@g.harvard.edu>
2017-05-29 22:41:21 -0400

    more modular; starting to parse reads

--- proatac/cli.py
+++ proatac/cli.py
@@ -12,18 +12,20 @@ from pkg_resources import get_distribution
 from subprocess import call, check_call
 from .proatacHelp import *
 
-# -----------------------
-# Core project option
-# -----------------------
+# ----------------------------------
+# Core object for handling the .yaml
+# ----------------------------------
 
 class proatacProject():
-	def __init__(self, project_yaml_file_handle, script_dir):
+	def __init__(self, yaml, script_dir):
 		
 		# Basic attributes
-		self.yaml = parse_manifest(project_yaml_file_handle)
+		self.yaml = yaml
 		self.name = self.yaml['project_name']
 		self.project_dir = self.yaml['project_dir']
 		self.analysis_person = self.yaml['analysis_person']
+		
+		
 		
 		# ------------------------------
 		# Make folders / log files
@@ -39,6 +41,8 @@ class proatacProject():
 			os.makedirs(logfolder)
 		logf = open(logfolder + "/base.proatac.log", 'a')
 		
+		
+		
 		# ------------------------------
 		# Process reference genome stuff
 		# ------------------------------
@@ -47,8 +51,22 @@ class proatacProject():
 		supported_genomes = ['hg19', 'hg38', 'mm9', 'mm10', 'hg19_mm10']
 		if any(self.reference_genome in s for s in supported_genomes):
 			click.echo(gettime() + "Found designated reference genome: %s" % self.reference_genome, logf)
+			self.tssFile = script_dir + "/anno/TSS/" + self.reference_genome + ".refGene.TSS.bed"
+			self.blacklistFile = script_dir + "/anno/blacklist/" + self.reference_genome + ".ful.blacklist.bed"
 		else: 
 			click.echo(gettime() + "Could not identify this reference genome: %s" % self.reference_genome, logf)
+				
+		if ("anno_files" in self.yaml['parameters']) and (str(self.yaml['parameters']['anno_files']) != "None"):
+			if "tss" in self.yaml['parameters']['anno_files']:
+				b = self.yaml['parameters']['anno_files']['tss']
+				if(b != ''):
+					self.tssFile = os.path.realpath(b)
+			if "blacklist" in self.yaml['parameters']['anno_files']:
+				b = self.yaml['parameters']['anno_files']['blacklist']
+				if(b != ''):
+					self.blacklistFile = os.path.realpath(b)
+		
+		
 		
 		# ------------------------
 		# Process dependency paths
@@ -59,7 +77,7 @@ class proatacProject():
 			self.bowtie2_path = self.yaml['paths']['bowtie2_path']
 		else:
 			self.bowtie2_path = shutil.which("bowtie2")
-		if(self.bowtie2_path == "None"):
+		if(str(self.bowtie2_path) == "None"):
 			sys.exit("ERROR: cannot find bowtie2 in environment; set the 'bowtie2_path' in the .yaml file or add to PATH")
 		
 		# bowtie2 index
@@ -73,7 +91,7 @@ class proatacProject():
 			self.macs2_path = self.yaml['paths']['macs2_path']
 		else:
 			self.macs2_path = shutil.which("macs2")
-		if(self.macs2_path == "None"):
+		if(str(self.macs2_path) == "None"):
 			sys.exit("ERROR: cannot find macs2 in environment; set the 'macs2_path' in the .yaml file or add to PATH")
 		
 		# samtools
@@ -81,40 +99,58 @@ class proatacProject():
 			self.samtools_path = self.yaml['paths']['samtools_path']
 		else:
 			self.samtools_path = shutil.which("samtools")
-		if(self.samtools_path == "None"):
-			sys.exit("ERROR: cannot find samtools in environment; set the 'samtools_path' in the .yaml file")
+		if(str(self.samtools_path) == "None"):
+			sys.exit("ERROR: cannot find samtools in environment; set the 'samtools_path' in the .yaml file or add to PATH")
 				
 		# R
 		if(self.yaml['paths']['R_path'] != ''):
 			self.R_path = self.yaml['paths']['R_path']
 		else:
 			self.R_path = shutil.which("R")
-		if(self.R_path == "None"):
-			sys.exit("ERROR: cannot find R in environment; set the 'R_path' in the .yaml file")
+		if(str(self.R_path) == "None"):
+			sys.exit("ERROR: cannot find R in environment; set the 'R_path' in the .yaml file or add to PATH")
+
+		# Java
+		if(self.yaml['paths']['java_path'] != ''):
+			self.java_path = self.yaml['paths']['java_path']
+		else:
+			self.java_path = shutil.which("java")
+		if(str(self.java_path) == "None"):
+			sys.exit("ERROR: cannot find Java in environment; set the 'java_path' in the .yaml file or add to PATH")
+						
 						
 		# Python3
 		if(self.yaml['paths']['python3_path'] != ''):
 			self.python3_path = self.yaml['paths']['python3_path']
 		else:
 			self.python3_path = shutil.which("python3")
-		if(self.python3_path == "None"):
-			sys.exit("ERROR: cannot find python3 in environment; set the 'python3_path' in the .yaml file")
+		if(str(self.python3_path) == "None"):
+			sys.exit("ERROR: cannot find python3 in environment; set the 'python3_path' in the .yaml file or add to PATH")
 		
 		# Check for R package dependencies
 		required_packages = ['ggplot2', 'tidyverse']
-		installed_packages = os.popen('''R -e "installed.packages()" | awk '{print $1}' | sort | uniq''').read().strip().split("\n")
+		installed_packages = os.popen(self.R_path + ''' -e "installed.packages()" | awk '{print $1}' | sort | uniq''').read().strip().split("\n")
 		if(not set(required_packages) < set(installed_packages)):
 			sys.exit("ERROR: cannot find the following R package: " + str(set(required_packages) - set(installed_packages)) + "\n" + 
-				"Install it in your R console and then try rerunning proatac.")
+				"Install it in your R console and then try rerunning proatac (but there may be other missing dependencies).")
+		
+		
 		
 		# ------------------------------
 		# Process sequencing directories
-		# ------------------------------
+		# ------------------------------	
 				
 		for run in self.yaml['sequencing_directories']:
+			process_seq_dir(run, logf)
 			print(run)
 		
-		
+		logf.close()
+
+
+
+# ------------------------------
+# Command line arguments
+# ------------------------------	
 
 @click.command()
 @click.version_option()
@@ -122,22 +158,31 @@ class proatacProject():
 @click.option('--stingy', is_flag=True, help='Space-efficient analyses; remove ')
 @click.argument('manifest')
 
+
+
+# ------------------------------
+# Main method for analysis
+# ------------------------------	
+
 def main(manifest, check, stingy):
 	"""Preprocessing ATAC and scATAC Data."""
 	__version__ = get_distribution('proatac').version
 	script_dir = os.path.dirname(os.path.realpath(__file__))
 
 	click.echo(gettime() + "Starting proatac pipeline v%s" % __version__)
-	p = proatacProject(manifest, script_dir)
+	yaml = parse_manifest(manifest)
 	
 	# -------------------------------
 	# Utility functions and variables
 	# -------------------------------
 	
-	outfolder = os.path.abspath(p.project_dir) 
+	outfolder = os.path.abspath(yaml['project_dir']) 
 	logfolder = outfolder + "/logs"
 	logf = open(logfolder + "/base.proatac.log", 'a')
 	cwd = os.getcwd()
+	
+	# Main Project Variable
+	p = proatacProject(yaml, script_dir)
 	
 	# -------------------------------
 	# Atypical analysis modes
--- proatac/proatacHelp.py
+++ proatac/proatacHelp.py
@@ -12,6 +12,7 @@ def string_hamming_distance(str1, str2):
     """
     return sum(itertools.imap(operator.ne, str1, str2))
 
+
 def rev_comp(seq):
     """
     Fast Reverse Compliment
@@ -20,14 +21,55 @@ def rev_comp(seq):
     return ''.join(tbl[s] for s in seq[::-1])
 
 def parse_manifest(manifest):
-    samples = []
-    if manifest.endswith(('.yaml', '.yml')):
-        with open(manifest, 'r') as f: 
-            m = yaml.load(f)
-        return m
-    else:
-        click.echo(gettime() + "Please specify a valid .yaml file for analysis")
+	"""
+	Basic function to parse yaml/yml
+	"""
+	samples = []
+	if manifest.endswith(('.yaml', '.yml')):
+		with open(manifest, 'r') as f: 
+			m = yaml.load(f)
+		return m
+	else:
+		click.echo(gettime() + "Please specify a valid .yaml file for analysis")
 
-def gettime(): # Matches `date` in Linux
+def gettime(): 
+	"""
+	Matches `date` in Linux
+	"""
 	return(time.strftime("%a ") + time.strftime("%b ") + time.strftime("%d ") + time.strftime("%X ") + 
 		time.strftime("%Z ") + time.strftime("%Y")+ ": ")
+		
+
+def process_seq_dir(d, logf):
+	"""
+	Function that takes a dictionary parsed from the main .yaml
+	and returns something more coherent to be processed downstream
+	"""
+	name = d['name']
+	version = d['version']
+	directory = d['dir']
+	
+	samples = []
+	
+	if 'remove_samples' in d:
+		remove_samples = d['remove_samples']
+	else:
+		remove_samples = ""
+	
+	if 'keep_samples' in d:
+		keep_samples = d['keep_samples']
+	else:
+		keep_samples = ""
+	
+	
+	if(keep_samples != ""):
+		keeplist = keep_samples.split(",")
+	
+	if(remove_samples != ""):
+		igslist = ignore_samples.split(",")
+		for byesample in igslist:
+			if byesample in samples: samples.remove(byesample)
+	
+	
+	
+	
