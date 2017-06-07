76d28919e42ff50c160b7e5bf3434c1fcd703436
caleblareau <caleblareau@g.harvard.edu>
2017-06-07 04:31:52 -0400

    more html slashing

--- docs/conf.py
+++ docs/conf.py
@@ -151,7 +151,7 @@ html_favicon = 'assets/favicon.ico'
 # Custom sidebar templates, maps document names to template names.
 #html_sidebars = {}
 html_sidebars = {
-    '**': ['localtoc.html', 'searchbox.html'],
+    '**': ['searchbox.html'],
 }
 
 # Additional templates that should be rendered to pages, maps page names to
