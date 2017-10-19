825dd5d90c6a676ad13773805a3cb89dc055c0e0
caleblareau <caleblareau@g.harvard.edu>
2017-10-19 12:33:38 -0400

    fixed error in bam counts processing

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dependencies = ['click', 'cutadapt', 'Numpy', 'editdistance', 'pytest', 'python-
 
 setup(
     name='proatac',
-    version='0.4.0',
+    version='0.4.1',
     url='https://github.com/buenrostrolab/proatac',
     license='MIT',
     author='Caleb Lareau',
