804114ec40e1f921052f8b5d56eb1c8ad40e7f0b
caleblareau <caleblareau@g.harvard.edu>
2018-06-07 16:47:52 -0400

    additional

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ dependencies = ['click', 'cutadapt', 'Numpy', 'editdistance', 'pytest', 'python-
 
 setup(
     name='proatac',
-    version='0.4.3',
+    version='0.4.4',
     url='https://github.com/buenrostrolab/proatac',
     license='MIT',
     author='Caleb Lareau',
