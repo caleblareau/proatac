7bfac8b12fef677268e04e641d7b0f9441c1622a
Caleb Lareau <caleblareau@g.harvard.edu>
2020-04-26 21:26:06 -0400

    tweak keep chrs

--- proatac/bin/python/oneProatacSample.py
+++ proatac/bin/python/oneProatacSample.py
@@ -25,7 +25,7 @@ mode = config["mode"]
 vs = config["very_sensitive"]
 
 # Ned to update
-keepchrs = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX"]
+keepchrs = ["chr1", "chr2", "chr3", "chr4", "chr5", "chr6", "chr7", "chr8", "chr9", "chr10", "chr11", "chr12", "chr13", "chr14", "chr15", "chr16", "chr17", "chr18", "chr19", "chr20", "chr21", "chr22", "chrX", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "X"]
 mitochrs = ['chrM', 'MT', 'humanM', 'mouseM', 'humanMT', 'mouseMT']
 read_quality = "30"
 ncores = "4"
